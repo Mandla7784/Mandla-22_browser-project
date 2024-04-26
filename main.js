29122280fa7c566c96706ed1b7887a15bb46c600
Mandla Dyonase <Marsatti7784@gmail.com>
2024-04-26 10:20:31 +0000

    Added a remove Duplicate function

--- main.js
+++ main.js
@@ -6,31 +6,34 @@ const [saveButton, deleteButton, cancelButton] = [
   return document.getElementById(id);
 });
 
-//elements with a class name
+// elements with a class name
 const form = document.querySelector(".modal");
+const modalOverlay = document.querySelector(".over-lay");
 const [boardsButtons, headerName, addNewTaskButton] = [
   ".boards button",
   ".header-name",
-
   ".btn-add-new-task",
 ].map((selector) => document.querySelectorAll(selector));
 
-//save and display tasks to UI
-
+// save and display tasks to UI
 const tasks = [];
- const description = document.querySelector("#desc-input").value;
- const tittle = document.querySelector("#title-input").value;
-
- 
-const task = {
-  id: "1",
-  title: tittle,
-  description:description
-};
 
 const saveAndDisplayTasks = function () {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const description = document.querySelector("#desc-input").value;
+    const title = document.querySelector("#title-input").value;
+
+    const task = {
+      id: "1",
+      title: title,
+      description: description,
+    };
+
+    tasks.push(task);
+    const uniqueTasks = removeDuplicates(tasks);
+    localStorage.setItem("tasks", JSON.stringify(uniqueTasks));
+    displayTasks(uniqueTasks);
   });
 
   addNewTaskButton[0].onclick = () => {
@@ -38,13 +41,35 @@ const saveAndDisplayTasks = function () {
     modalOverlay.style.display = "flex";
   };
 
-  cancelButton.onclick = () => {
-    const modalOverlay = document.querySelector(".over-lay");
+  saveButton.onclick = () => {
     modalOverlay.style.display = "none";
-    tasks.push(task);
-    const uniqueTsk = [...new Set(tasks)]
-    localStorage.setItem('tasks',JSON.stringify(uniqueTsk))
   };
 };
 
 window.onload = saveAndDisplayTasks;
+
+function displayTasks() {
+  const taskContainer = document.querySelector(".task");
+  taskContainer.innerHTML = "";
+  const fetchedTasks = JSON.parse(localStorage.getItem("tasks"));
+  fetchedTasks.forEach((task) => {
+    const taskElement = document.createElement("div");
+    taskElement.innerHTML = `
+      <p>${task.title}</p>
+      <p>${task.description}</p>
+    `;
+    taskContainer.appendChild(taskElement);
+  });
+}
+
+function removeDuplicates(arr) {
+  const uniqueArray = [];
+  const map = new Map();
+  for (const item of arr) {
+    if (!map.has(item.id)) {
+      map.set(item.id, true);
+      uniqueArray.push(item);
+    }
+  }
+  return uniqueArray;
+}
